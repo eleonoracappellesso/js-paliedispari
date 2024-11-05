02c1feb44d938f47178ab69bb08c66586ca049c4
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-05 14:47:16 +0100

    created a function wordisPalindroma to verify the word

--- script/script.js
+++ script/script.js
@@ -11,15 +11,30 @@ Creare una funzione per capire se la parola inserita è palindroma
 let word = prompt("Inserisci una parola:");
 console.log(word);
 
-// rimuovo gli spazi e trasformo la parola in minuscolo
-word = word.replace(/\s+/g, '').toLowerCase();
+// // rimuovo gli spazi e trasformo la parola in minuscolo
+// word = word.replace(/\s+/g, '').toLowerCase();
 
-// trasformo la stringa in array, la inverto e la faccio tornare di tipo stringa
-let reversedWord = word.split('').reverse().join('');
+// // trasformo la stringa in array, la inverto e la faccio tornare di tipo stringa
+// let reversedWord = word.split('').reverse().join('');
+// console.log(reversedWord);
 
 // verifico se la parola è palindroma
-if(word === reversedWord){
-    console.log('La parola scelta è palindroma!')
-} else {
-    console.log('La parola scalta non è palindroma!')
+// if(word === reversedWord){
+//     console.log('La parola scelta è palindroma!')
+// } else {
+//     console.log('La parola scelta non è palindroma!')
+// }
+
+//creo una funzione per verificare la parola
+function wordisPalindroma (word) {
+    word = word.replace(/\s+/g, '').toLowerCase();
+    let reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
 }
+
+// verifico se la parola è palindroma
+if(wordisPalindroma(word)){
+     console.log('La parola scelta è palindroma!')
+ } else {
+         console.log('La parola scelta non è palindroma!')
+ }
