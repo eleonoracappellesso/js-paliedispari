f1baf14b5c94c7172822d7425149895329600a64
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-05 15:33:12 +0100

    created functions randomNumber and verifyEvenOdd

--- script/script.js
+++ script/script.js
@@ -26,15 +26,45 @@ console.log(word);
 // }
 
 //creo una funzione per verificare la parola
-function wordisPalindroma (word) {
+function wordisPalindroma(word) {
     word = word.replace(/\s+/g, '').toLowerCase();
     let reversedWord = word.split('').reverse().join('');
     return word === reversedWord;
 }
 
 // verifico se la parola è palindroma
-if(wordisPalindroma(word)){
-     console.log('La parola scelta è palindroma!')
- } else {
-         console.log('La parola scelta non è palindroma!')
- }
+if (wordisPalindroma(word)) {
+    console.log('La parola scelta è palindroma!')
+} else {
+    console.log('La parola scelta non è palindroma!')
+}
+
+
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+// chiedo all'utente di scegliere pari o dispari
+let evenOdd = prompt('Scegli pari o dispari?').toLowerCase();
+
+
+// creo una funzione per generare un numero random tra 1 e 5
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+// creo una funzione per verificare se un numero è pari o dispari
+function verifyEvenOdd(number) {
+    if(number % 2 === 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
+
+
