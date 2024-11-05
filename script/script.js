a941b8b07e98bfc4c69447986897b4187b7c76bb
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-05 14:29:23 +0100

    define the variables

--- script/script.js
+++ script/script.js
@@ -1,2 +1,19 @@
 "use strict";
 console.clear();
+
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// chiedo all'utente una parola
+let word = prompt("Inserisci una parola:");
+console.log(word);
+
+// rimuovo gli spazi e trasformo la parola in minuscolo
+word = word.replace(/\s+/g, '').toLowerCase();
+
+// trasformo la stringa in array, la inverto e la faccio tornare di tipo stringa
+let reversedWord = word.split('').reverse().join('');
+
